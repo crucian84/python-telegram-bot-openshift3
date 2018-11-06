ee196cb830bbe4fc41344a09f8c3d0b3188c3c16
crucian84 <ukraine270@gmail.com>
2018-11-06 12:10:14 +0600

    add comments

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def convert(bot, update, args):
     #try:
         #dollars = int(update.message.text)
         #dollars = args
-    tenge = int(args) * 373
+    tenge = int('10') * 373
     #except (NameError, SyntaxError, ValueError):
     #    tenge = "Введите целое число"
     bot.send_message(chat_id=update.message.chat_id, text=tenge)
